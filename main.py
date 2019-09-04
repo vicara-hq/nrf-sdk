7634da4600dddd6c6b1f65ebc099f5ba18190a43
Rishit Bansal <thebongy@pop-os.localdomain>
2019-09-05 02:35:16 +0530

    fix: list branches

--- main.py
+++ main.py
@@ -55,8 +55,6 @@ def main():
 
     logging.info("Found following versions of nrf sdk:\n{}".format(" ".join(sdk_downloads.keys())))
 
-    run_shell_command("git fetch --all")
-    
     for version in sdk_downloads:
         logging.info("Downloading {}".format(version))
 
